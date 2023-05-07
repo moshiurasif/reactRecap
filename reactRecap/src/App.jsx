2db046ea9932b7d14289b3ec58243a8077dd5410
asif2212 <asifmd2212@gamil.com>
2023-05-07 07:09:22 +0600

    useState practice

--- reactRecap/src/App.jsx
+++ reactRecap/src/App.jsx
@@ -1,3 +1,4 @@
+import { useState } from 'react'
 import './App.css'
 
 function App() {
@@ -6,10 +7,24 @@ function App() {
   return (
     <>
       <div>
-        <h1>Hello React</h1>
+        <Counter></Counter>
       </div>
      
     </>
+  )
+}
+
+function Counter(){
+  const [count, setCount] = useState(0);
+  // const increaseCount = () =>setCount(count + 1);
+  
+  // const decreaseCount = () =>setCount(count - 1);
+  return (
+    <div>
+      <h2>Count: {count}</h2>
+      <button onClick= {() =>setCount(count + 1)}>Increase</button>
+      <button onClick= {() =>setCount(count - 1)}>Decrease</button>
+    </div>
   )
 }
 
